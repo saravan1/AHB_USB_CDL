3becb35cffc434429947129b8947dd117419410c
Lohith Chittineni <lchittin@purdue.edu>
2021-04-24 18:08:51 -0400

    started top level

--- source/ahb_lite_usb.sv
+++ source/ahb_lite_usb.sv
@@ -10,14 +10,34 @@ module ahb_lite_usb
 (
     input wire clk,
     input wire n_rst,
+    input wire[3:0] haddr,
+    input wire hsel,
+    input wire[1:0] htrans,
+    input wire[1:0] hsize,
+    input wire hwrite, 
+    input wire[31:0] hwdata,
     input wire dplus_in,
     input wire dminus_in,
-    input wire haddr,
-    input wire hsel,
-    input wire 
 
+    output reg[31:0] hrdata,
+    output reg hready,
+    output reg hresp,
+    output reg dplus_out,
+    output reg dminus_out,
 );
 
+// AHB LITE MODULE
+
+
+// DATA BUFFER MODULE
+
+
+
+// USB TX MODULE
+
+
+
+// USB RX MODULE
 
 
 endmodule
